6ec6adc52cf6834ed190f06ff677374c13c468ee
Luis Torres <luistjr@gmail.com>
2020-09-17 13:06:01 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -5,9 +5,7 @@ current_time = Time.now
 if (current_time.to_i / 2) == 0 
   puts "Even!"
   
-elsif !(current_time.to_i / 2 == 0)
+else !(current_time.to_i / 2 == 0)
   puts "Odd!"
-
-else 
   
 end 
